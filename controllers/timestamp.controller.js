8df9830835928949e5df18021cfcf4b9f6523d84
Mai <truongkimmai2004@nguyendudalat.edu.vn>
2024-04-24 15:26:44 +0700

    fix error

--- controllers/timestamp.controller.js
+++ controllers/timestamp.controller.js
@@ -1,31 +1,39 @@
 const formatDate = require('../services/formatTime')
 
 const timeStamp = async (req, res) => {
-
-    if (req.params.date.includes('-')) {
-        const date = new Date(req.params.date)
-        if (isNaN(date.getTime())) {
-            res.json({ error: 'Invalid Date' })
-        } else {
-            const utc = formatDate(date)
-            res.json({
-                unix: Date.parse(date),
-                utc: utc
-            })
-        }
+    if (!req.params.date) {
+        const date = new Date()
+        const utc = formatDate(date)
+        res.json({
+            unix: Date.parse(date),
+            utc: utc
+        })
     } else {
-        const unix = Number(req.params.date)
-        const date = new Date(unix);
-        if (isNaN(date.getTime())) {
-            res.json({ error: 'Invalid Date' })
+        if (req.params.date.includes('-')) {
+            const date = new Date(req.params.date)
+            if (isNaN(date.getTime())) {
+                res.json({ error: 'Invalid Date' })
+            } else {
+                const utc = formatDate(date)
+                res.json({
+                    unix: Date.parse(date),
+                    utc: utc
+                })
+            }
         } else {
-            const utc = formatDate(date);
-            res.json({
-                unix: unix,
-                utc: utc
-            })
+            const unix = Number(req.params.date)
+            const date = new Date(unix);
+            if (isNaN(date.getTime())) {
+                res.json({ error: 'Invalid Date' })
+            } else {
+                const utc = formatDate(date);
+                res.json({
+                    unix: unix,
+                    utc: utc
+                })
+            }
         }
-    }
+    }   
 }
 
 module.exports = timeStamp
