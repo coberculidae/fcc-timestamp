72edce0e886e51bccd3f71cb2977f4aa15b6c7da
Mai <truongkimmai2004@nguyendudalat.edu.vn>
2024-04-24 15:15:54 +0700

    fix error

--- controllers/timestamp.controller.js
+++ controllers/timestamp.controller.js
@@ -1,24 +1,30 @@
 const formatDate = require('../services/formatTime')
 
 const timeStamp = async (req, res) => {
-if (req.params.date.includes('-')) {
+
+    if (req.params.date.includes('-')) {
         const date = new Date(req.params.date)
-        console.log(date)
-        const utc = formatDate(date)
-        res.json({
-            unix: Date.parse(date),
-            utc: utc
-        })
+        if (isNaN(date.getTime())) {
+            res.json({ error: 'Invalid Date' })
+        } else {
+            const utc = formatDate(date)
+            res.json({
+                unix: Date.parse(date),
+                utc: utc
+            })
+        }
     } else {
-        const unixNumber = Number(req.params.date)
-        const date = new Date(unixNumber);
-        console.log(date)
-        const utc = formatDate(date);
-        const unix = req.params.date;
-        res.json({
-            unix: unix,
-            utc: utc
-        })
+        const unix = Number(req.params.date)
+        const date = new Date(unix);
+        if (isNaN(date.getTime())) {
+            res.json({ error: 'Invalid Date' })
+        } else {
+            const utc = formatDate(date);
+            res.json({
+                unix: unix,
+                utc: utc
+            })
+        }
     }
 }
 
