bd500e29ef957e051e7c583a489a1816cfdc3367
Mai <truongkimmai2004@nguyendudalat.edu.vn>
2024-04-24 14:55:38 +0700

    fix unix

--- controllers/timestamp.controller.js
+++ controllers/timestamp.controller.js
@@ -1,22 +1,25 @@
 const formatDate = require('../services/formatTime')
 
 const timeStamp = async (req, res) => {
-    if (req.params.date.includes('-')) {
+if (req.params.date.includes('-')) {
         const date = new Date(req.params.date)
+        console.log(date)
         const utc = formatDate(date)
         res.json({
             unix: Date.parse(date),
             utc: utc
         })
     } else {
-        const date = new Date(req.params.date / 1000)
-        const utc = formatDate(date)
+        const unixNumber = Number(req.params.date)
+        const date = new Date(unixNumber);
+        console.log(date)
+        const utc = formatDate(date);
+        const unix = req.params.date;
         res.json({
-            unix: req.params.date,
+            unix: unix,
             utc: utc
         })
     }
-
 }
 
 module.exports = timeStamp
