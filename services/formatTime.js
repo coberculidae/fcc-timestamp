fb23a4c9d105125a085a6d4fde47471e7346a0e5
Mai <truongkimmai2004@nguyendudalat.edu.vn>
2024-04-24 15:30:54 +0700

    fix error

--- services/formatTime.js
+++ services/formatTime.js
@@ -2,9 +2,9 @@ const formatTime = (time) => {
     const week = ['','Mon','Tue','Wed','Thu','Fri','Sat','Sun'];
     const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     const day = String(week[time.getUTCDay()]);
-    const date = String(time.getUTCDate());
+    const date = String(time.getUTCDate()).padStart(2, '0');
     const month = String(months[time.getUTCMonth()])
-    const year = String(time.getUTCFullYear());
+    const year = String(time.getUTCFullYear()).padStart(4, '0');
     const hours = String(time.getUTCHours()).padStart(2, '0');
     const minutes = String(time.getUTCMinutes()).padStart(2, '0');
     const seconds = String(time.getUTCSeconds()).padStart(2, '0');
